9be74168c16c615f84938330b5065ea9a99f1c8e
Andres Rojas <and9193ro@gmail.com>
2025-09-06 20:41:54 -0500

    Update Order.java
    Order actualizado segun tarea

--- ShopNow/src/Model/Order.java
+++ ShopNow/src/Model/Order.java
@@ -1,16 +1,18 @@
 package Model;
-import java.util.*;
 import java.util.ArrayList;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Order {
     private int IdOrder;
     private ArrayList<Product> products = new ArrayList<>();
-    private Date BuyDate;
-    
+    private LocalDateTime buyDate;
 
     public Order(int IdOrder, Client client) {
         this.IdOrder = IdOrder;
         this.products = new ArrayList<>();
+        this.buyDate = LocalDateTime.now();
     }
 
     public void addProduct(Product newProduct) {
@@ -23,6 +25,24 @@ public class Order {
             totalCost = totalCost + products.get(i).getCost();
         }
         return totalCost;
+    }
+    public void showOrder() {
+        System.out.println("Pedido N°: " + IdOrder);
+
+        for (Product p : products) {
+            System.out.println(" - " + p.getNameProduct() + " ($" + p.getCost() + ")");
+        }
+
+        System.out.println("Total: $" + totalCost());
+
+        // Formato de fecha en español
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE d 'de' MMMM 'de' yyyy. HH:mm:ss", new Locale("es", "ES"));
+
+        // Mostrar fecha de compra
+        System.out.println("Fecha: " + buyDate.format(formatter));
+
+        // Mostrar fecha de pago estimada (24 horas después)
+        System.out.println("Fecha de pago estimada: " + buyDate.plusHours(24).format(formatter));
     }
 
     public int getIdOrder() {
